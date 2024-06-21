d49335199f209412e4669da771a3a922c21e24eb
Giulio De Pasquale <git@depasquale.giugl.io>
2024-06-21 12:03:02 +0200

    feat(bfg): merge BFG AWS with DO

--- bfg9000.py
+++ bfg9000.py
@@ -16,6 +16,7 @@ from modules.aws import (
     InvalidAMIIDNotFound,
     InvalidAMIMalformed,
 )
+from modules.digital_ocean import DigitalOceanWrapper
 from modules.logger import Logger, StatDB
 
 # Get the absolute path to the directory containing current script
@@ -52,7 +53,7 @@ def spawn_process(
 # collect facts and generate <many, many...> problems
 
 
-def extract_facts(ami: str, working_directory: Path) -> bool:
+def extract_facts_aws(ami: str, working_directory: Path) -> bool:
     # the users are ordered. Prefer less privileged first
     default_ssh_users = ["ec2-user", "bitnami", "ubuntu", "admin"]
     facts_extractor: Path = SCRIPT_DIR / "facts_extractor.py"
@@ -266,12 +267,142 @@ def solve_problem(problem: Path, working_directory: Path):
             STAT_DB.update_run_state(StatDB.RunState.SOLUTION_NOT_FOUND)
 
 
-def upload_and_run_script(ami: str, script: Path):
+def extract_facts_do(ami: str, working_directory: Path) -> bool:
+    # the users are ordered. Prefer less privileged first
+    default_ssh_users = [DigitalOceanWrapper.DEFAULT_USER]
+    facts_extractor: Path = SCRIPT_DIR / "facts_extractor.py"
+    domain: Path = SCRIPT_DIR / "domain.pddl"
+    pickle_file: Path = SCRIPT_DIR / f"extractor_data_{ami}.pkl"
+
+    if pickle_file.exists():
+        LOGGER.info(f"Pickle file for {ami} already exists")
+        return False
+
+    try:
+        with DigitalOceanWrapper(ami, stat_db=STAT_DB) as aw:
+            if aw.stat_db:
+                aw.stat_db.update_run_state(StatDB.RunState.SPAWNING)
+            aw.wait_for_instance()
+            if aw.stat_db:
+                aw.stat_db.update_run_state(StatDB.RunState.INSTANCE_SPAWNED)
+
+            LOGGER.info("Connecting via SSH...")
+
+            ssh_user: str = None
+            for user in default_ssh_users:
+                try:
+                    LOGGER.info(f"Trying to connect to instance as {user}...")
+                    aw.connect_ssh(user)
+
+                    LOGGER.info("Connected to the instance.")
+                    ssh_user = user
+
+                    if aw.stat_db:
+                        aw.stat_db.update_run_state(StatDB.RunState.SSH_CONNECTED)
+
+                    break
+                except Exception as e:
+                    LOGGER.error(f"Could not connect: {e}")
+
+                    if aw.stat_db:
+                        aw.stat_db.update_run_state(StatDB.RunState.SSH_FAILED)
+
+                    ssh_user: str = None
+
+                    continue
+
+            if not ssh_user:
+                LOGGER.error("Could not login as any user to this instance. Abort.")
+
+                if aw.stat_db:
+                    aw.stat_db.update_run_state(StatDB.RunState.SSH_FAILED)
+
+                return False
+
+            instance = aw.instance
+
+            start_time: float = time.time()
+
+            if args.fc:
+                spawn_process(
+                    [
+                        facts_extractor,
+                        "-d",
+                        domain,
+                        "-p",
+                        "22",
+                        "-s",
+                        "-t",
+                        instance.public_dns_name,
+                        "-u",
+                        ssh_user,
+                        "-k",
+                        aw.ssh_private_key,
+                        "-n",
+                        ami,
+                        "-fc",
+                    ],
+                    working_directory=working_directory,
+                )
+
+            else:
+                spawn_process(
+                    [
+                        facts_extractor,
+                        "-d",
+                        domain,
+                        "-p",
+                        "22",
+                        "-s",
+                        "-t",
+                        instance.public_dns_name,
+                        "-u",
+                        ssh_user,
+                        "-k",
+                        DigitalOceanWrapper.ENV_KEY_PATH,
+                        "-n",
+                        ami,
+                    ],
+                    working_directory=working_directory,
+                )
+
+            # add problem generation time to StatDB
+            end_time: float = time.time()
+            elapsed_time: float = end_time - start_time
+
+            if STAT_DB:
+                STAT_DB.update_problem_generation_time(elapsed_time)
+
+            # check if the facts extractor generated the problems
+            # if so, log it in the StatDB
+            if not Path(Path(working_directory) / f"generated_problems_{ami}").exists():
+                LOGGER.error("Could not extract facts.")
+
+                if aw.stat_db:
+                    aw.stat_db.update_run_state(StatDB.RunState.FACTS_FAILED)
+
+                return False
+
+            LOGGER.info("Facts extracted!")
+
+            if aw.stat_db:
+                aw.stat_db.update_run_state(StatDB.RunState.FACTS_EXTRACTED)
+    except Exception as e:
+        LOGGER.error(f"Caught general exception while spawning instance: {e}")
+        if STAT_DB:
+            STAT_DB.update_run_state(StatDB.RunState.PRE_SPAWN_FAILURE)
+
+        raise e
+
+    return True
+
+
+def upload_and_run_script(cls, ami: str, script: Path):
     if not script.exists():
         LOGGER.error(f"{script} does not exist.")
         return
 
-    with AWSWrapper(ami) as aw:
+    with cls(ami) as aw:
         aw.wait_for_instance()
 
         try:
@@ -298,13 +429,13 @@ def upload_and_run_script(ami: str, script: Path):
     LOGGER.info("Done")
 
 
-def handle_aws(args):
+def handle_do(args):
     ami: str = args.ami
 
     # if a script is provided
     # upload to remote and execute
     if args.s:
-        upload_and_run_script(ami, Path(args.s))
+        upload_and_run_script(DigitalOceanWrapper, ami, Path(args.s))
         return
 
     temp_dir = Path(mkdtemp(prefix=f"bfg_{ami}_"))
@@ -313,7 +444,7 @@ def handle_aws(args):
     LOGGER.info("Extracting facts...")
 
     try:
-        facts_extracted = extract_facts(ami, temp_dir)
+        facts_extracted = extract_facts_do(ami, temp_dir)
     except Exception as e:
         LOGGER.error(f"Caught exception: {e}")
 
@@ -336,12 +467,51 @@ def handle_aws(args):
         LOGGER.error(f"Caught exception: {e}")
 
 
+def handle_aws(args):
+    ami: str = args.ami
+
+    # if a script is provided
+    # upload to remote and execute
+    if args.s:
+        upload_and_run_script(AWSWrapper, ami, Path(args.s))
+        return
+
+    temp_dir = Path(mkdtemp(prefix=f"bfg_{ami}_"))
+
+    LOGGER.info(f"Using temporary directory {temp_dir}")
+    LOGGER.info("Extracting facts...")
+
+    try:
+        facts_extracted = extract_facts_aws(ami, temp_dir)
+    except Exception as e:
+        LOGGER.error(f"Caught exception: {e}")
+
+        exit(-1)
+
+    if not facts_extracted:
+        exit(-1)
+
+    root_problem: Path = Path(
+        temp_dir
+        / Path(f"generated_problems_{ami}")
+        / Path("micronix-problem-root.pddl")
+    )
+
+    LOGGER.info("Solving root problem...")
+
+    try:
+        solve_problem(root_problem, temp_dir)
+    except Exception as e:
+        LOGGER.error(f"Caught exception: {e}")
+
 
 # TODO: organize functionalities
 def main(args):
     match args.command:
         case "aws":
             handle_aws(args)
+        case "do":
+            handle_do(args)
         case "extract":
             print("Extractor not handled yet")
         case _:
@@ -443,6 +613,31 @@ def parse_arguments():
         "-fc",
         action="store_true",
         help="If set, the program will use CVEs related to the binary on the AWS instance without checking if they are patched or not.",
+    )
+
+    # Create the parser for the "do" command
+    do_parser = subparsers.add_parser(
+        "do", help="Spawn, extract system information, generate problems, and attempt to solve a problem in an end-to-end scenario from a Digital Ocean instance."
+    )
+
+    # Create a subcommand group for Digital Ocean
+    do_group = do_parser.add_argument_group(
+        "Digital Ocean connector", "Arguments for connecting to a Digital Ocean instance"
+    )
+    do_group.add_argument(
+        "ami",
+        type=str,
+        help="The Digital Ocean instance image to connect to."
+    )
+    do_group.add_argument(
+        "-s",
+        type=str,
+        help="The path to an executable script to be uploaded and run on the Digital Ocean instance."
+    )
+    do_group.add_argument(
+        "-fc",
+        action="store_true",
+        help="If set, the program will use CVEs related to the binary on the Digital Ocean instance without checking if they are patched or not.",
     )
 
     return parser.parse_args()
