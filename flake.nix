63d17f43c1f647e64e60c6cdddb3260ace9f9e29
Giulio De Pasquale <git@depasquale.giugl.io>
2024-07-16 21:29:45 +0100

    fix(flake/python): do not use poetry in shell, let Nix handle everything

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
     powerlifted-flake.url = "github:peperunas/planner8";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, powerlifted-flake }:
+  outputs = { self, nixpkgs, flake-utils, powerlifted-flake, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -13,18 +17,37 @@
           config.allowUnfree = true;
         };
 
+        ponix = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+        inherit (ponix) mkPoetryEnv;
+
         powerlifted = powerlifted-flake.packages.${system}.powerlifted;
 
-        shellWithPkgs = packages: pkgs.mkShell {
-          inherit packages;
-          buildInputs = [ pkgs.bashInteractive ];
-          shellHook = ''
-            poetry install && poetry shell
-          '';
-        };
-
+        shellWithPkgs = packages:
+          pkgs.mkShell {
+            inherit packages;
+            buildInputs = [
+              (mkPoetryEnv {
+                projectDir = self;
+                python = pkgs.python312;
+                preferWheels = true;
+                overrides = ponix.overrides.withDefaults (self: super: {
+                  bcrypt = super.bcrypt.override (old: {
+                    preferWheel = false;
+                  });
+                  gcs-oauth2-boto-plugin = super.gcs-oauth2-boto-plugin.overridePythonAttrs (old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                  });
+                  gsutil = super.gsutil.overridePythonAttrs (old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                  });
+                  python-gflags = super.python-gflags.overridePythonAttrs (old: {
+                    buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                  });
+                });
+              })
+            ];
+          };
         shell = with pkgs; shellWithPkgs [
-          (poetry.override { python3 = python311; })
           powerlifted
           parallel-full
           sqlite
