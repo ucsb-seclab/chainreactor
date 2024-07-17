18deaa8189ca2f15c75e071ee254c81d2332d1f8
Gabriel Pizarro <gpizarro@javaman.net>
2024-07-17 13:53:58 -0700

    rewrite bfg argparse, add extract functionality (WIP)

--- bfg9000.py
+++ bfg9000.py
@@ -6,6 +6,8 @@ import time
 
 from pathlib import Path
 from tempfile import mkdtemp
+import dill
+from pddl.formatter import problem_to_string
 
 from modules.aws import (
     AWSWrapper,
@@ -18,18 +20,22 @@ from modules.aws import (
 )
 from modules.digital_ocean import DigitalOceanWrapper
 from modules.logger import Logger, StatDB
+from modules.connectors import ListenConnector, RemoteConnector, SSHConnector
+from modules.extractor import FactsExtractor
+from modules.encoder import Encoder
 
-# Get the absolute path to the directory containing current script
 SCRIPT_DIR = Path(__file__).resolve().parent
-# to be set later
-STAT_DB: StatDB = None
-STAT_DB_PATH: Path = Path(SCRIPT_DIR / "stats.sqlite")
+STAT_DB: StatDB = None  # Initialized later
+STAT_DB_PATH: Path = SCRIPT_DIR / "stats.sqlite"
 LOGGER = Logger(__name__, stat_db_path=STAT_DB_PATH, log_to_file=True)
+DOMAIN_PATH: Path = SCRIPT_DIR / "domain.pddl"
 
 
 def init_statdb(ami, fc):
+    global STAT_DB
     STAT_DB = StatDB(STAT_DB_PATH, ami, fc)
-    
+
+
 def spawn_process(
     cmd, stream_stdout: bool = True, working_directory: Path = None
 ) -> tuple[list[str], list[str]]:
@@ -512,82 +518,201 @@ def handle_aws(args):
         LOGGER.error(f"Caught exception: {e}")
 
 
-# TODO: organize functionalities
-def main(args):
-    match args.command:
-        case "aws":
-            handle_aws(args)
-        case "do":
-            handle_do(args)
-        case _:
-            print("Unknown command")
+def add_subparser_extract(subparsers):
+    parser = subparsers.add_parser(
+        "extract",
+        help="Extract system information, generate problems, and attempt to solve from a custom connection",
+    )
+
+    group = parser.add_argument_group("extract")
+    group.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        help="Port to connect or listen on (depending on -r, -l or SSH)",
+        required=True,
+    )
+    group.add_argument(
+        "-t",
+        "--target",
+        type=str,
+        help="Target to connect to (to be used with -r or SSH)",
+    )
+    group.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        help="Filesystem name for the results (pickled facts and PDDL problems)",
+    )
+    group.add_argument(
+        "-uc",
+        "--unpatched-cves",
+        action="store_true",
+        help="If set, assume CVEs in remote binaries are unpatched",
+    )
+
+    connection_group = group.add_mutually_exclusive_group(required=True)
+    connection_group.add_argument(
+        "-l",
+        "--listen",
+        action="store_true",
+        help="Listen for reverse shell connection instead of connecting to host",
+    )
+    connection_group.add_argument(
+        "-r",
+        "--reverse",
+        action="store_true",
+        help="Connect back to host's exposed shell",
+    )
+    connection_group.add_argument(
+        "-s",
+        "--ssh",
+        action="store_true",
+        help="Connect to the host via SSH",
+    )
+
+    group.add_argument(
+        "-u",
+        "--user",
+        type=str,
+        help="SSH user",
+    )
+    group.add_argument(
+        "-k",
+        "--key",
+        type=str,
+        help="SSH private key",
+    )
 
 
-def parse_arguments():
-    # Create the top-level parser
+def add_subparser_cloud(subparsers):
+    parser = subparsers.add_parser(
+        "cloud",
+        help="Spawn, extract system information, generate problems, and attempt to solve a cloud-provided instance",
+    )
+
+    group = parser.add_argument_group("cloud")
+    group.add_argument(
+        "provider",
+        choices=["aws", "do"],
+        help="Which cloud provider to use (aws=Amazon, do=Digital Ocean)",
+    )
+    group.add_argument(
+        "image",
+        type=str,
+        help="The provider-specific image ID to spawn",
+    )
+    group.add_argument(
+        "-s",
+        "--script",
+        type=str,
+        help="A local script to be executed on the remote instance",
+    )
+    group.add_argument(
+        "-uc",
+        "--unpatched-cves",
+        action="store_true",
+        help="If set, assume CVEs in remote binaries are unpatched",
+    )
+
+
+def validate_args_extract(args):
+    if args.port not in range(1, 2**16):
+        LOGGER.error(f"Invalid port: {args.port}")
+        exit(-1)
+
+    if args.ssh:
+        if not args.target:
+            LOGGER.error(
+                "Please specify a target (-t/--target) when using SSH (-s/--ssh)"
+            )
+            exit(-1)
+        if not args.user:
+            LOGGER.error("Please specify a user (-u/--user) when using SSH (-s/--ssh)")
+            exit(-1)
+        if not args.key:
+            LOGGER.error("Please specify a key (-k/--key) when using SSH (-s/--ssh)")
+            exit(-1)
+        args.key = Path(args.key)
+        if not args.key.exists():
+            LOGGER.error(f"Key file {args.key} does not exist")
+            exit(-1)
+
+    elif args.reverse:
+        if not args.target:
+            LOGGER.error(
+                "Please specify a target (-t/--target) when using reverse shell (-r/--reverse)"
+            )
+            exit(-1)
+
+
+def handle_extract(args):
+    validate_args_extract(args)
+
+    facts_pickle = Path("extractor_data.pkl")
+    problems_dir = Path("generated_problems")
+    if args.name:
+        facts_pickle = facts_pickle.with_stem(f"{facts_pickle.stem}_{args.name}")
+        problems_dir = problems_dir.with_stem(f"{problems_dir.stem}_{args.name}")
+    problems_dir.mkdir(parents=True, exist_ok=True)
+
+    # Use cached facts or extract them
+    if facts_pickle.exists():
+        LOGGER.info("Loading pickled facts...")
+        with open(facts_pickle, "rb") as f:
+            facts_container = dill.load(f)
+    else:
+        LOGGER.info("Extracting facts...")
+        if args.ssh:
+            connector = SSHConnector(args.target, args.user, args.key)
+        elif args.listen:
+            connector = ListenConnector(args.port)
+        elif args.reverse:
+            connector = RemoteConnector(args.target, args.port)
+        connector.initialize()
+
+        facts_extractor = FactsExtractor(connector)
+        facts_extractor.extract(args.unpatched_cves)
+        facts_container = facts_extractor.container
+
+        LOGGER.info(f"Pickling facts to {facts_pickle}")
+        with open(facts_pickle, "wb") as f:
+            dill.dump(facts_container, f)
+
+    # Generate problem files
+    encoder = Encoder(facts_container)
+    problems = encoder.generate_problems(DOMAIN_PATH)
+    for name, problem in problems.items():
+        problem_file = problems_dir / f"{name}.pddl"
+        with open(problem_file, "w") as f:
+            f.write(problem_to_string(problem))
+        LOGGER.info(f"Generated problem {problem_file}")
+
+
+def validate_args_cloud(args):
+    print(args)
+
+
+def handle_cloud(args):
+    validate_args_cloud(args)
+
+
+def main():
     parser = argparse.ArgumentParser()
-
-    # Create subparsers for different commands
     subparsers = parser.add_subparsers(dest="command", required=True)
+    add_subparser_extract(subparsers)
+    add_subparser_cloud(subparsers)
 
-    # Create the parser for the "aws" command
-    aws_parser = subparsers.add_parser(
-        "aws", help="Spawn, extract system information, generate problems, and attempt to solve a problem in an end-to-end scenario from an AWS AMI instance."
-    )
-
-    # Create a subcommand group for AWS
-    aws_group = aws_parser.add_argument_group(
-        "AWS connector", "Arguments for connecting to an AWS instance"
-    )
-    aws_group.add_argument(
-        "ami",
-        type=str,
-        help="The Amazon Machine Image (AMI) ID of the AWS instance to connect to."
-    )
-    aws_group.add_argument(
-        "-s",
-        type=str,
-        help="The path to an executable script to be uploaded and run on the AWS instance."
-    )
-    aws_group.add_argument(
-        "-fc",
-        action="store_true",
-        help="If set, the program will use CVEs related to the binary on the AWS instance without checking if they are patched or not.",
-    )
-
-    # Create the parser for the "do" command
-    do_parser = subparsers.add_parser(
-        "do", help="Spawn, extract system information, generate problems, and attempt to solve a problem in an end-to-end scenario from a Digital Ocean instance."
-    )
-
-    # Create a subcommand group for Digital Ocean
-    do_group = do_parser.add_argument_group(
-        "Digital Ocean connector", "Arguments for connecting to a Digital Ocean instance"
-    )
-    do_group.add_argument(
-        "ami",
-        type=str,
-        help="The Digital Ocean instance image to connect to."
-    )
-    do_group.add_argument(
-        "-s",
-        type=str,
-        help="The path to an executable script to be uploaded and run on the Digital Ocean instance."
-    )
-    do_group.add_argument(
-        "-fc",
-        action="store_true",
-        help="If set, the program will use CVEs related to the binary on the Digital Ocean instance without checking if they are patched or not.",
-    )
-
-    return parser.parse_args()
+    args = parser.parse_args()
+    if args.command == "extract":
+        handle_extract(args)
+    elif args.command == "cloud":
+        handle_cloud(args)
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
-
     try:
-        main(args)
+        main()
     finally:
         if STAT_DB:
             STAT_DB.end_run()
